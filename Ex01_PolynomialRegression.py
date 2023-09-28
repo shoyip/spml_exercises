bf7b5559b7acfd7aca6dac2414dcbecff7994c75
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2023-09-28 12:19:03 +0200

    arrived at point 4

--- Ex01_PolynomialRegression.py
+++ Ex01_PolynomialRegression.py
@@ -29,17 +29,36 @@ sigma = 0
 
 # (2) generate 10 pairs of (x, y) with ZERO SIGMA and x in the range [0, 1]
 # (3) fit the data
+print("=== (1-3) Generate paris and fit data")
+
 x1 = rng.uniform(0, 1, 10)
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     for func_name, func in zip(['A', 'B'], [funcA, funcB]):
-        print(f'\n=== Printing results for function {func_name}\n')
+        print(f'\n--- Printing results for function {func_name}\n')
         
         y1 = getY(func, mu, sigma, x1)
         
         p1 = np.polynomial.Polynomial.fit(x1, y1, deg=1)
         p3 = np.polynomial.Polynomial.fit(x1, y1, deg=3)
         p10 = np.polynomial.Polynomial.fit(x1, y1, deg=10)
+    
+        print(p1, p3, p10, sep='\n\n')
+
+print("\n=== (4) generate other 20 pairs\n")
+
+x2 = rng.uniform(0, 1.25, 20)
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    for func_name, func in zip(['A', 'B'], [funcA, funcB]):
+        print(f'\n--- Printing results for function {func_name}\n')
+        
+        y2 = getY(func, mu, sigma, x2)
+        
+        p1 = np.polynomial.Polynomial.fit(x2, y2, deg=1)
+        p3 = np.polynomial.Polynomial.fit(x2, y2, deg=3)
+        p10 = np.polynomial.Polynomial.fit(x2, y2, deg=10)
     
         print(p1, p3, p10, sep='\n\n')
