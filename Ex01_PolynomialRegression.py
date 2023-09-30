b530940794d9944faae168c9e4e8a216b2ffe807
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2023-09-30 16:04:18 +0200

    created workspace dirs

--- Ex01_PolynomialRegression.py
+++ Ex01_PolynomialRegression.py
@@ -1,11 +1,11 @@
 # Exercise 01: Polynomial Regression
 # ==================================
 
-from scipy.optimize import curve_fit
 import numpy as np
+import matplotlib.pyplot as plt
 import warnings
 
-rng = np.random.default_rng()
+rng = np.random.default_rng(42)
 
 # Defining the (hidden) functions to generate the data
 
@@ -29,36 +29,42 @@ sigma = 0
 
 # (2) generate 10 pairs of (x, y) with ZERO SIGMA and x in the range [0, 1]
 # (3) fit the data
-print("=== (1-3) Generate paris and fit data")
 
-x1 = rng.uniform(0, 1, 10)
+def get_dataset(x_min, x_max, n_pairs, mu, sigma):
+    x = rng.uniform(x_min, x_max, n_pairs)
+    y_dict = {}
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    for func_name, func in zip(['A', 'B'], [funcA, funcB]):
-        print(f'\n--- Printing results for function {func_name}\n')
-        
-        y1 = getY(func, mu, sigma, x1)
-        
-        p1 = np.polynomial.Polynomial.fit(x1, y1, deg=1)
-        p3 = np.polynomial.Polynomial.fit(x1, y1, deg=3)
-        p10 = np.polynomial.Polynomial.fit(x1, y1, deg=10)
-    
-        print(p1, p3, p10, sep='\n\n')
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        for func_name, func in zip(['A', 'B'], [funcA, funcB]):
+            y = getY(func, mu, sigma, x)
+            y_dict[func_name] = y
 
-print("\n=== (4) generate other 20 pairs\n")
+    return x, y_dict
 
-x2 = rng.uniform(0, 1.25, 20)
+def get_polyfunc(deg, func_name):
+    x, ys = get_dataset(0, 1, 10, 0, 0)
+    y = ys[func_name]
+    p = np.polynomial.Polynomial.fit(x, y, deg=deg)
+    coefs = p.convert().coef
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    for func_name, func in zip(['A', 'B'], [funcA, funcB]):
-        print(f'\n--- Printing results for function {func_name}\n')
-        
-        y2 = getY(func, mu, sigma, x2)
-        
-        p1 = np.polynomial.Polynomial.fit(x2, y2, deg=1)
-        p3 = np.polynomial.Polynomial.fit(x2, y2, deg=3)
-        p10 = np.polynomial.Polynomial.fit(x2, y2, deg=10)
-    
-        print(p1, p3, p10, sep='\n\n')
+    return coefs
+
+def get_polyxy(x_min, x_max, deg, func_name):
+    coefs = get_polyfunc(deg, func_name)
+    x = np.linspace(x_min, x_max, 100)
+    y = np.polynomial.polynomial.polyval(x, coefs, tensor=False)
+    return (x, y)
+
+x_t, ys_t = get_dataset(0, 1.25, 20, 0, 1)
+x_c, y_c = get_polyxy(0, 1.25, 3, 'A')
+
+for func_name in ['A', 'B']:
+    plt.figure()
+    plt.scatter(x_t, ys_t[func_name], s=3)
+    for deg in [1, 3, 10]:
+        x_c, y_c = get_polyxy(0, 1.25, deg, func_name)
+        plt.plot(x_c, y_c, label=f'Order {deg}')
+    plt.legend()
+    plt.show()
+    plt.close()
