5b85bb1bb9744b944e7b374ce3e373f78abc7ea4
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2023-10-26 18:44:00 +0200

    corrected exercise 3 to have 1000 train+test, no pdf

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 # use as: ./generate.sh 02
 # where 02 is the notebook id
 
-ipynb_file=$(ls notebooks | grep $1 | head -n1)
+ipynb_file=$(ls notebooks | grep $1 | grep ipynb | head -n1)
 stem="${ipynb_file%%.*}"
 quarto convert notebooks/$ipynb_file --output qmds/$stem.qmd
 quarto render qmds/$stem.qmd -M echo:false --to pdf
